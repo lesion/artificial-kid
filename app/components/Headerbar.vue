5439f3d828591de4acd65ee5cef12266223cd3e2
lesion <lesion@autistici.org>
2016-07-06 16:33:39 +0200

    headerbar

---
+++ app/components/Headerbar.vue
@@ -0,0 +1,26 @@
+<template lang='pug'>
+  ui-toolbar(hide-nav-icon,flat)
+      span &nbsp;&nbsp;&nbsp;
+      a(href='#') ciao 
+      span  / 
+      a(href='#') prova
+      a {{ cwd }}
+      .actions(slot='actions')
+        ui-icon-button(icon='settings',type='flat')
+        ui-textbox(placeholder='search',icon-right)
+</template>
+<script>
+import actions from '../vuex/actions'
+
+export default {
+  vuex: {
+    actions,
+    getters: {
+      cwd: state => state.cwd
+    }
+  }
+}
+</script>
+<style  lang='stylus' scoped>
+  
+</style>
