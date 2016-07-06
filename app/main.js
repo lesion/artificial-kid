a4aa5fff6005cacd58a1cc96790c6bd373490faa
lesion <lesion@autistici.org>
2016-07-06 16:33:56 +0200

    app/main.js

--- app/main.js
+++ app/main.js
@@ -4,9 +4,13 @@ import App from './App'
 import Keen from 'keen-ui'
 import 'keen-ui/dist/min/keen-ui.min.css'
 
+import store from './vuex/store'
+
 Vue.use(Keen)
+
 /* eslint-disable no-new */
 new Vue({
+  store,
   el: 'body',
   components: { App }
 })
